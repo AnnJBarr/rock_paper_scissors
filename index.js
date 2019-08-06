058af8b07358e2fd96c9cb14838ae1f11c795d02
AnnJBarr <annbarr@hotmail.com>
2019-08-06 08:27:16 +0100

    Game logic added

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const readline = require("readline").createInterface({
 
 function handleUserResponse(response){
     const options = ['rock', 'paper', 'scissors'];
+    console.log(response.toLowerCase());
     //Ignore capitalisation of words
-    if (response.toLowerCase() !='rock' && response.toLowerCase() !='paper' && response.toLowerCase() != 'scissor') 
-            {
-             console.log("Please choose one only of rock, paper or scissors!");
+    if (!(response.toLowerCase() ==='rock' || response.toLowerCase() ==='paper' ||  response.toLowerCase() === 'scissor') )
+            {var notvalid = true;
+            console.log("Your response is not valid");
              //readline.question("Please choose again!\n", response); Want to get them to answer again - this doesn't work
-            }
-
+            } else { var notvalid = false
+                console.log("I like your response");}
+            console.log(notvalid);
     
     //Validate user choice and tell them if they made a mistake
 
@@ -26,15 +28,29 @@ function handleUserResponse(response){
     console.log(randomint);
     var compplay = options[randomint];
     console.log("Computer chose: ", compplay);
-
+    console.log("Player chose: ", response);
+    
     //Compare the two choices
     //Rock wins over scissors
     //Scissors win over paper
     //Paper wins over rock
-
     //Report on who won
+    if (notvalid == false){
+        if (response.toLowerCase() === compplay) {
+            console.log("It's a draw!")
+        } else {
+            if (response.toLowerCase() === 'rock' && compplay === 'scissors'
+                || response.toLowerCase() === 'scissors' && compplay === 'paper'
+                || response.toLowerCase() === 'paper' && compplay === 'rock') {
+                    console.log("Player wins!")
+                } else console.log("Computer wins!");
+        }
+    } else {
+        console.log("You must choose one of rock, paper or scissors - please play again");
+    }    
+    
 
-    console.log("Player chose: ", response);
+    
     readline.close();
     }
 
