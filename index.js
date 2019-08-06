e907b7cd72d2a8e1e74a2dc194f25d1f4bda70d7
AnnJBarr <annbarr@hotmail.com>
2019-08-06 17:12:08 +0100

    tidying up messages and adding colour

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const readline = require("readline").createInterface({
 
 function handleUserResponse(response){
     const options = ['rock', 'paper', 'scissors'];
-    console.log(response.toLowerCase());
+    //console.log(response.toLowerCase()); in for testing only
     //Ignore capitalisation of words
     if (!(response.toLowerCase() ==='rock' || response.toLowerCase() ==='paper' ||  response.toLowerCase() === 'scissors') )
             {var notvalid = true;
-            console.log("Your response is not valid");
+            console.log("Your response is not valid :(");
              //readline.question("Please choose again!\n", response); Want to get them to answer again - this doesn't work
             } else { var notvalid = false
-                console.log("I like your response");}
+                console.log("I like your response :)");}
             //console.log(notvalid);
     
     //Validate user choice and tell them if they made a mistake
@@ -25,7 +25,7 @@ function handleUserResponse(response){
 
     //Make a random selection for the computer
     var randomint = Math.floor(Math.random() * (3));
-    console.log(randomint);
+    //console.log(randomint); in for testing only
     var compplay = options[randomint];
     console.log("Computer chose: ", compplay);
     console.log("Player chose: ", response);
@@ -37,13 +37,13 @@ function handleUserResponse(response){
     //Report on who won
     if (notvalid == false){
         if (response.toLowerCase() === compplay) {
-            console.log("\x1b[33m", "It's a draw!")
+            console.log("\x1b[33m", "It's a draw!", "\x1b[37m")
         } else {
             if (response.toLowerCase() === 'rock' && compplay === 'scissors'
                 || response.toLowerCase() === 'scissors' && compplay === 'paper'
                 || response.toLowerCase() === 'paper' && compplay === 'rock') {
-                    console.log("\x1b[35m", "Player wins!")
-                } else console.log("\x1b[36m", "Computer wins!");
+                    console.log("\x1b[35m", "Player wins!", "\x1b[37m")
+                } else console.log("\x1b[36m", "Computer wins!", "\x1b[37m");
         }
     } else {
         console.log("You must choose one of rock, paper or scissors - please play again");
